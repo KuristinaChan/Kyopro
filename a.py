a7f2297448446b7c666fb9170b24889ee8fb43f9
KuristinaChan <aristocracycorp@gmail.com>
2024-08-18 17:01:34 +0530

    first

---
+++ a.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# get a integer
+a = int(input())
+# get two integers separated with half-width break
+b, c = map(int, input().split())
+# get a string
+s = input()
+# output
+print("{} {}".format(a+b+c, s))

6073299cb14e423381072ff130d726e96e082dbe
KuristinaChan <aristocracycorp@gmail.com>
2024-08-27 22:03:37 +0530

    Wrong submission for move the coins

---
+++ Probability_and_Game_Theory/Move_The_Coins.cpp
@@ -0,0 +1,316 @@
+#ifndef DEBUG_TEMPLATE_CPP
+#define DEBUG_TEMPLATE_CPP
+
+
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+
+namespace __DEBUG_UTIL__
+{
+    using namespace std;
+    /* Primitive Datatypes Print */
+    void print(const char *x) { cerr << x; }
+    void print(bool x) { cerr << (x ? "T" : "F"); }
+    void print(char x) { cerr << '\'' << x << '\''; }
+    void print(signed short int x) { cerr << x; }
+    void print(unsigned short int x) { cerr << x; }
+    void print(signed int x) { cerr << x; }
+    void print(unsigned int x) { cerr << x; }
+    void print(signed long int x) { cerr << x; }
+    void print(unsigned long int x) { cerr << x; }
+    void print(signed long long int x) { cerr << x; }
+    void print(unsigned long long int x) { cerr << x; }
+    void print(float x) { cerr << x; }
+    void print(double x) { cerr << x; }
+    void print(long double x) { cerr << x; }
+    void print(string x) { cerr << '\"' << x << '\"'; }
+    template <size_t N>
+    void print(bitset<N> x) { cerr << x; }
+    void print(vector<bool> v)
+    { /* Overloaded this because stl optimizes vector<bool> by using
+          _Bit_reference instead of bool to conserve space. */
+        int f = 0;
+        cerr << '{';
+        for (auto &&i : v)
+            cerr << (f++ ? "," : "") << (i ? "T" : "F");
+        cerr << "}";
+    }
+    /* Templates Declarations to support nested datatypes */
+    template <typename T>
+    void print(T &&x);
+    template <typename T>
+    void print(vector<vector<T>> mat);
+    template <typename T, size_t N, size_t M>
+    void print(T (&mat)[N][M]);
+    template <typename F, typename S>
+    void print(pair<F, S> x);
+    template <typename T, size_t N>
+    struct Tuple;
+    template <typename T>
+    struct Tuple<T, 1>;
+    template <typename... Args>
+    void print(tuple<Args...> t);
+    template <typename... T>
+    void print(priority_queue<T...> pq);
+    template <typename T>
+    void print(stack<T> st);
+    template <typename T>
+    void print(queue<T> q);
+    /* Template Datatypes Definitions */
+    template <typename T>
+    void print(T &&x)
+    {
+        /*  This works for every container that supports range-based loop
+            i.e. vector, set, map, oset, omap, dequeue */
+        int f = 0;
+        cerr << '{';
+        for (auto &&i : x)
+            cerr << (f++ ? "," : ""), print(i);
+        cerr << "}";
+    }
+    template <typename T>
+    void print(vector<vector<T>> mat)
+    {
+        int f = 0;
+        cerr << "\n~~~~~\n";
+        for (auto &&i : mat)
+        {
+            cerr << setw(2) << left << f++, print(i), cerr << "\n";
+        }
+        cerr << "~~~~~\n";
+    }
+    template <typename T, size_t N, size_t M>
+    void print(T (&mat)[N][M])
+    {
+        int f = 0;
+        cerr << "\n~~~~~\n";
+        for (auto &&i : mat)
+        {
+            cerr << setw(2) << left << f++, print(i), cerr << "\n";
+        }
+        cerr << "~~~~~\n";
+    }
+    template <typename F, typename S>
+    void print(pair<F, S> x)
+    {
+        cerr << '(';
+        print(x.first);
+        cerr << ',';
+        print(x.second);
+        cerr << ')';
+    }
+    template <typename T, size_t N>
+    struct Tuple
+    {
+        static void printTuple(T t)
+        {
+            Tuple<T, N - 1>::printTuple(t);
+            cerr << ",", print(get<N - 1>(t));
+        }
+    };
+    template <typename T>
+    struct Tuple<T, 1>
+    {
+        static void printTuple(T t) { print(get<0>(t)); }
+    };
+    template <typename... Args>
+    void print(tuple<Args...> t)
+    {
+        cerr << "(";
+        Tuple<decltype(t), sizeof...(Args)>::printTuple(t);
+        cerr << ")";
+    }
+    template <typename... T>
+    void print(priority_queue<T...> pq)
+    {
+        int f = 0;
+        cerr << '{';
+        while (!pq.empty())
+            cerr << (f++ ? "," : ""), print(pq.top()), pq.pop();
+        cerr << "}";
+    }
+    template <typename T>
+    void print(stack<T> st)
+    {
+        int f = 0;
+        cerr << '{';
+        while (!st.empty())
+            cerr << (f++ ? "," : ""), print(st.top()), st.pop();
+        cerr << "}";
+    }
+    template <typename T>
+    void print(queue<T> q)
+    {
+        int f = 0;
+        cerr << '{';
+        while (!q.empty())
+            cerr << (f++ ? "," : ""), print(q.front()), q.pop();
+        cerr << "}";
+    }
+    /* Printer functions */
+    void printer(const char *) {} /* Base Recursive */
+    template <typename T, typename... V>
+    void printer(const char *names, T &&head, V &&...tail)
+    {
+        /* Using && to capture both lvalues and rvalues */
+        int i = 0;
+        for (size_t bracket = 0; names[i] != '\0' and (names[i] != ',' or bracket != 0); i++)
+            if (names[i] == '(' or names[i] == '<' or names[i] == '{')
+                bracket++;
+            else if (names[i] == ')' or names[i] == '>' or names[i] == '}')
+                bracket--;
+        cerr.write(names, i) << " = ";
+        print(head);
+        if (sizeof...(tail))
+            cerr << " ||", printer(names + i + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+    /* PrinterArr */
+    void printerArr(const char *) {} /* Base Recursive */
+    template <typename T, typename... V>
+    void printerArr(const char *names, T arr[], size_t N, V... tail)
+    {
+        size_t ind = 0;
+        for (; names[ind] and names[ind] != ','; ind++)
+            cerr << names[ind];
+        for (ind++; names[ind] and names[ind] != ','; ind++)
+            ;
+        cerr << " = {";
+        for (size_t i = 0; i < N; i++)
+            cerr << (i ? "," : ""), print(arr[i]);
+        cerr << "}";
+        if (sizeof...(tail))
+            cerr << " ||", printerArr(names + ind + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) std::cerr << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+#define debugArr(...) std::cerr << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+#endif
+
+#define kuristina() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define si set<int>
+#define sll set<long long>
+#define sc set<char>
+#define vc vector<char>
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repb(i,a,b) for(int i=a;i<=b;i++)
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)(x).size()
+#define ff first
+#define ss second
+#define ln endl
+
+#define mp make_pair
+#define pqueue priority_queue
+#define pb push_back
+
+ll gcd(const ll a, const ll b){return(b==0?a:gcd(b,a%b));}
+ll lcm(const ll a, const ll b){return a/gcd(a,b)*b;}
+ll binpow(ll a,ll n){ll ans=1;for(;n;n/=2,a*=a)if(n&1)ans*=a;return ans;}
+ll binpow(ll a,ll n,const ll&mod){ll ans=1;for(;n;n/=2,a=a*a%mod)if(n&1)ans=ans*a%mod;return ans;}
+ll cdiv(const ll&a,const ll&b){return(a/b+((a^b)>0 && a%b));}
+ll fdiv(const ll&a,const ll&b){return a/b-((a^b)<0 && a%b);}
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+const int nax = 50050;
+int n;
+int coins[nax] = {0};
+vi adj[nax];
+int q;
+int x;//x is the nim sum
+int level[nax];
+bool visited[nax];
+int p[nax];
+void bfs(int start) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+    level[start] = 0;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                level[neighbor] = level[node] + 1;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+void solve(){
+     cin>>n;
+     rep(i,0,n){
+        cin>>coins[i];
+     }
+     rep(i,0,n-1){
+        int u,v;
+        cin>>u>>v;
+        adj[--u].pb(--v);
+        p[v]=u;
+     }
+     p[0]=-1;
+     bfs(0);
+     cin>>q;
+     int a,b;
+     rep(i,0,n){
+        if((level[i]&1)){
+            x^=coins[i];
+        }
+     }
+     while(q--){
+        cin>>a>>b;
+        --a;--b;
+        int i = b;
+        bool flag = true;
+        while(i!=-1){
+            if(i==a){
+                cout<<"INVALID"<<ln;
+                flag = false;
+                break;
+            }
+            i=p[i];
+        }
+        if(!flag){
+            continue;
+        }
+        int new_level = level[b]+1;
+        ll cur = x;
+        if(level[a]%2 != new_level%2){
+            cur^=coins[a];
+        }
+        if(cur>0)cout<<"YES"<<ln;
+        else cout<<"NO"<<ln;
+     }
+     return;
+}
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    #endif
+    kuristina();
+
+    solve();
+}
